6a615af8dd02a16e72e2741c04cffc4ad4aa2377
tinmarino <tinmarino@gmail.com>
2018-08-19 13:30:27 +0200

    Script : wiki2html to get a clean markdown (without spaces or with indent)

--- wiki2html.sh
+++ wiki2html.sh
@@ -24,6 +24,27 @@ fi
 
 munix(){
 sed -r 's/(\[.+\])\(([^#)]+)\)/\1(\2.html)/g' <"$INPUT" | \
+  # Double the new line before code
+  perl -0pe 's/((^|\n\S)[^\n]*)\n\t([^*])/\1\n\n\t\3/g;' | \
+  perl -0pe '
+    # begining of line
+    $bol = "(?:^|\\n)";
+    # Pillar is a line starting at bof
+    $pillar = "(?:(?:$bol)\\S[^\\n]*|\$)" ;
+    # Chrimp start of line 
+    sub chin {
+      my $r = $2;
+      $r =~ s/\n\t/\n/g ;
+      return $r;
+    };
+    # delete the start of line (\t) for list items
+    s/($pillar)\n\t\*(.*?)(?=$pillar)/$1 . "\n\n*" . chin($2)/ges
+  ' | \
+  # Remove void lines
+  perl -lpe 's/^\s*$//' | \
+
+  tee test2.md | \
+  # Compile
 	pandoc $MATH -s -f $SYNTAX -t html -c $CSSFILENAME | \
 	sed -r 's/<li>(.*)\[ \]/<li class="todo done0">\1/g; s/<li>(.*)\[X\]/<li class="todo done4">\1/g' \
 	>"$OUTPUT.html"
@@ -40,6 +61,9 @@ sed -r 's/(\[.+\])\(([^)]+)\)/\1(\2.html)/g' <"$INPUT" | \
 }
 
 mchoose(){
+
+# Choose which compiler : in termux no pandoc yet
+# Better no to compile on termux
 tmp=$(uname -a)
 
 shopt -s nocasematch
@@ -49,4 +73,5 @@ else
 	munix
 fi
 }
+
 mchoose
