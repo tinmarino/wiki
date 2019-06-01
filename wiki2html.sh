c48232e9c3ce0f34c191e9fee665dc9bf1ee5006
tinmarino <tinmarino@gmail.com>
2019-06-01 22:53:44 +0200

    Sh : wiki2html simplify : I am obliged to do real markdown -> 4 spaces for code and not one tab

--- wiki2html.sh
+++ wiki2html.sh
@@ -23,32 +23,15 @@ fi
 # >&2 echo "MATH: $MATH"
 
 munix(){
-sed -r 's/(\[.+\])\(([^#)]+)\)/\1(\2.html)/g' <"$INPUT" | \
-  # Double the new line before code
-  perl -0pe 's/((^|\n\S)[^\n]*)\n\t([^*])/\1\n\n\t\3/g;' | \
-  # 
-  perl -0pe '
-    # begining of line
-    $bol = "(?:^|\\n)";
-    # Pillar is a line starting at bof
-    $pillar = "(?:(?:$bol)\\S[^\\n]*|\$)" ;
-    # Chrimp start of line 
-    sub chin {
-      my $r = $2;
-      $r =~ s/\n\t/\n/g ;
-      return $r;
-    };
-    # delete the start of line (\t) for list items
-    s/($pillar)\n\t\*(.*?)(?=$pillar)/$1 . "\n\n*" . chin($2)/ges
-  ' | \
-  # Remove void lines
-  perl -lpe 's/^\s*$//' | \
-
-  tee test2.md | \
-  # Compile
-	pandoc $MATH -s -f $SYNTAX -t html -c $CSSFILENAME | \
-	sed -r 's/<li>(.*)\[ \]/<li class="todo done0">\1/g; s/<li>(.*)\[X\]/<li class="todo done4">\1/g' \
-	>"$OUTPUT.html"
+sed -r 's/(\[.+\])\(([^#)]+)\)/\1(\2.html)/g' <"$INPUT" \
+  `# Double the new line before code` \
+  | perl -0pe 's/((^|\n\S)[^\n]*)\n\t([^*])/\1\n\n\t\3/g;' \
+  `# Remove spaces in void lines` \
+  | perl -lpe 's/^\s*$//' \
+  `# Compile` \
+	| pandoc $MATH -s -f $SYNTAX -t html -T $FILE -c $CSSFILENAME \
+	`# | sed -r 's/<li>(.*)\[ \]/<li class="todo done0">\1/g; s/<li>(.*)\[X\]/<li class="todo done4">\1/g'` \
+>"$OUTPUT.html"
 }
 
 mtermux(){
