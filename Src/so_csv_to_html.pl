f88281e29442a2a558bbb8b2c9d14c46b49e506a
tinmarino <tinmarino@gmail.com>
2019-10-23 14:18:33 +0200

    Src: Add So scripts from Software/Perl/Datamining (I removed from there to avoid duplicates)

---
+++ Src/so_csv_to_html.pl
@@ -0,0 +1,142 @@
+#!/usr/bin/perl
+=content
+So: Convert cvs <- sql to md -> html
+
+CSV (use in $row->[0]):
+    0ID, 1Title, 2Score, 3,4,5question,score,date
+    6,7,8answer1,score,date 9,10,11answser2, 12,13,14answer3
+
+Ex: pl csv_to_md.pl so_perl_top_1000.csv
+
+TODO: get language in a variable
+=cut
+
+use strict; use warnings; use v5.26;
+use Text::CSV;
+
+
+sub arg_to_str {
+    # Garde foux pour terminer les font formating si line dropped by a mal interpreted \
+    my $row_suffix = "</b> </em> </i> </small> </strong> </sub> </sup>";
+
+    # Open
+    open my $fh, "<:encoding(utf8)", $ARGV[0] or die "Usage: $0: $!";
+
+    # Init
+    my $csv = Text::CSV->new ( { binary => 1 } )  # should set binary attribute.
+                    or die "Cannot use CSV: ".Text::CSV->error_diag ();
+
+    ## Discard the fisrt line
+    $csv->getline( $fh );
+    my @res;
+    my $ln = 0;
+    while ( my $row = $csv->getline( $fh ) ) {
+        $ln++;
+        my $q_year = (split /-/, $row->[5])[0];
+        my $line = ""
+            . "\n" . $row_suffix . "\n\n"
+            . "### $ln: $row->[1] (score [$row->[2]](https://stackoverflow.com/q/$row->[0]) in $q_year)\n"
+            . "\n#### Question\n$row->[3]";
+        if ($row->[6]) { $line .= "\n#### Answer accepted (score $row->[7])\n$row->[6]" ;}
+        if ($row->[9]) { $line .= "\n#### Answer 2 (score $row->[10])\n$row->[9]";}
+        if ($row->[12]) { $line .= "\n#### Answer 3 (score $row->[13])\n$row->[12]";}
+        push @res, $line;
+    }
+    $csv->eof or $csv->error_diag();
+    close $fh;
+    return @res;
+}
+
+
+# Treat a single post in md
+sub treat {
+    # In
+    my $post = shift;
+
+    # Code perl as variable
+    $post =~ s|<pre[^>]*><code>|```perl\n|g;
+    $post =~ s|</code></pre>|```|g;
+    $post =~ s|<code>(.*?)</code>|`$1`|g;
+
+    # Paragraph
+    $post =~ s|^(\s*)<p>(.*?)</p>|$1$2  |mg;
+    $post =~ s|^</?p>$||mg;
+
+    # Headings
+    $post =~ s|<h[1234]>|<h5>|g;
+    $post =~ s|<h/[1234]>|</h5>|g;
+
+    # Characters
+    ## TODO Avoid some windows path to cut end of line then make all it
+    ## but not in code blocks
+
+    # Prefix
+    return $post;
+}
+
+
+sub dump_md {
+    # In
+    my $posts = shift;
+    my $path = shift;
+    open my $fh, ">:encoding(utf8)", $path or die "Write faile: $path: $!";
+
+    # Add header (as a post)
+    my $title = "Perl SO ${\(scalar @$posts)} most viewed";
+    my $header = <<"END_HEADER";
+---
+title: $title
+category: Perl
+css: ../Css/code_prism_dark.css
+css: ../Css/color_dark_solarized.css
+header-includes: <script type="text/javascript" src="../Css/js_masonry_desandro.js"></script>
+---
+
+<section class="level2">
+END_HEADER
+    my $footer = <<"END_FOOTER";
+</section>
+END_FOOTER
+    unshift @$posts, $header;
+    push @$posts, $footer;
+
+
+    # Out
+    say $fh @$posts;
+    close $fh;
+
+    # Return
+    return $title;
+}
+
+
+
+sub main {
+    say 'Read cvs';
+    my @in = arg_to_str;
+
+    say 'Treat';
+    my @out;
+    for my $post (@in) {
+        $post = treat $post;
+        push @out, $post;
+    }
+
+    say 'Write md';
+    my $md_path = $ARGV[0] =~ s/\.[^.]*/.md/r;
+    my $html_path = $ARGV[0] =~ s/\.[^.]*/.html/r;
+    my $title = dump_md(\@out, $md_path);
+    $title = substr $title, 1;
+
+    say 'Pandoc html';
+    system "pandoc $md_path 
+        -t html
+        -f markdown-auto_identifiers
+        -o $html_path"
+        =~ s/\n/ /gr;
+
+    return;
+}
+
+# Go
+main();
---
+++ Src/so_get_to_view.sql
@@ -0,0 +1,71 @@
+-- Send me here: https://data.stackexchange.com/stackoverflow/query/new
+
+SELECT TOP 10
+-- Meta
+p.Id                 AS "id",
+min(p.Title)         AS "title",
+min(p.ViewCount)     AS "view",
+
+-- Question
+min(p.Body)          AS "question",
+min(p.Score)         AS "score_q",
+min(p.LastEditDate)  AS "date_q",
+
+-- Answsers
+min(a.Body)          AS "answer1",
+min(a.Score)         AS "score1",
+min(a.LastEditDate)  AS "date2",
+
+min(b.Body)          AS "answer2",
+min(b.Score)         AS "score2",
+min(b.LastEditDate)  AS "date2",
+
+min(c.Body)          AS "answer3",
+min(c.Score)         AS "score3",
+min(c.LastEditDate)  AS "date3"
+
+
+
+FROM Posts p
+
+-- Join Accepted answer
+left outer JOIN Posts a ON p.AcceptedAnswerId = a.Id
+
+-- Join best not a
+left outer JOIN Posts b
+    ON p.Id = b.ParentId 
+    AND (a.Id is null or b.Id is null or  b.Id != a.Id)
+AND b.Score =
+(
+    select max(n.Score) as maxscore
+    from Posts n
+    where n.ParentId = p.Id
+        AND (a.Id is null or n.Id != a.Id)
+)
+
+-- Join best not a not b
+left outer JOIN Posts c ON 
+	p.Id = c.ParentId
+    AND (a.Id is null or c.Id is null or  a.Id != c.Id)
+    AND (b.Id is null or c.Id is null or  b.Id != c.Id)
+    AND c.Score =
+(
+   select max(n.Score) as maxscore
+   from Posts n 
+   where n.ParentId = p.Id
+        AND (a.Id is null or n.Id != a.Id)
+        AND (b.Id is null or n.Id != b.Id)
+)
+
+-- Get tags
+inner join posttags pt on pt.postid = p.id
+inner join tags t on pt.tagid = t.id
+
+WHERE 
+t.tagname in ('perl')  -- tag
+AND p.PostTypeId = 1   -- select questions
+
+
+GROUP BY p.ID, p.ViewCount
+
+ORDER BY p.ViewCount DESC
